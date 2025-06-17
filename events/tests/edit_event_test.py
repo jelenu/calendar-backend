8f48a5b6a0020b74638c99c45f221b9cb529ca04
jelenu <jesusleon2700@gmail.com>
2025-06-17 13:35:00 +0200

    edit event tests completed

---
+++ events/tests/edit_event_test.py
@@ -0,0 +1,58 @@
+from accounts.models import CustomUser
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from events.models import Event
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils import timezone
+import datetime
+
+class EventEditTests(APITestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(email='testuser@example.com', password='testpass123', is_active=True)
+        self.user2 = CustomUser.objects.create_user(email='testuser2@example.com', password='testpass123', is_active=True)
+
+        start = timezone.make_aware(datetime.datetime(2025, 6, 17, 10, 0, 0))
+        end = timezone.make_aware(datetime.datetime(2025, 6, 17, 12, 0, 0))
+        self.event = Event.objects.create(
+            title="Initial Event",
+            description="Initial Description",
+            startDate=start,
+            endDate=end,
+            category="Initial Category",
+            user=self.user
+        )
+        self.url = reverse('event-detail', kwargs={'pk': self.event.pk})
+        self.updated_event_data = {
+            "title": "Updated Event",
+            "category": "Updated Category"
+        }
+
+    @staticmethod
+    def get_token_for_user(user):
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+
+    def test_edit_event_authenticated(self):
+        access_token = self.get_token_for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.patch(self.url, self.updated_event_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.event.refresh_from_db()
+        self.assertEqual(self.event.title, self.updated_event_data['title'])
+        self.assertEqual(self.event.category, self.updated_event_data['category'])
+
+    def test_edit_event_unauthenticated(self):
+        response = self.client.patch(self.url, self.updated_event_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_edit_event_not_owner(self):
+        access_token = self.get_token_for_user(self.user2)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        response = self.client.patch(self.url, self.updated_event_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.event.refresh_from_db()
+        self.assertNotEqual(self.event.title, self.updated_event_data['title'])
+        self.assertNotEqual(self.event.category, self.updated_event_data['category'])
