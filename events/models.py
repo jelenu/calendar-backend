b8fab1d5003d01a927369cddc08eb1f0c81da198
jelenu <jesusleon2700@gmail.com>
2025-06-19 10:56:02 +0200

    category now is a model

--- events/models.py
+++ events/models.py
@@ -2,17 +2,35 @@ from django.db import models
 import uuid
 from django.conf import settings
 
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="categories"
+    )
+
+    def __str__(self):
+        return self.name
+
 class Event(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     startDate = models.DateTimeField()
     endDate = models.DateTimeField()
-    category = models.CharField(max_length=100, blank=True, null=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="events"
+    )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="clients"
+        related_name="events"
     )
     createdAt = models.DateTimeField(auto_now_add=True)
 
